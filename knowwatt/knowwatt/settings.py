b9e8f1bc44d915e287041bbcdba4606fc353d0a3
= <r.nakinton@gmail.com>
2026-03-15 18:45:55 +0700

    change setting enable local sqlite development

--- knowwatt/knowwatt/settings.py
+++ knowwatt/knowwatt/settings.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
 import os
+from dotenv import load_dotenv
+load_dotenv() 
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -70,18 +73,24 @@ WSGI_APPLICATION = 'knowwatt.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB','knowwatt'),
-        'USER': os.environ.get('POSTGRES_USER','knowwatt_user'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD','your_password'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+if os.environ.get('USE_SQLITE') == 'True':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT'),
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
